8c225d6951cf446899efbdfd370b9850d2ec6496
RamanDhakar <72760906+RamanDhakar@users.noreply.github.com>
2025-10-17 00:01:18 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
   subscription_id = "c0bc73ee-a1a8-439e-87fe-61ec35aabd85"
   tenant_id = "9a4dbbe6-303b-455a-8eb4-41f2363a8436"
   client_id = "e14ef5b7-95aa-4fc2-a7df-dee0c143b3a2"
-  client_secret = var.Azure_secret
+  client_secret = var.Azure_secret.value
     features {}
 }
 
@@ -22,3 +22,4 @@ resource "azurerm_resource_group" "RGdetails" {
   
 
 }
+
